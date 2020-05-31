eeafabfa4083af0e5760d2299735d9b0976b6a2b
Bankole Ahmed <kidrolex19@gmail.com>
2020-05-31 20:47:49 +0100

    Removed extra chunks of codes :recycle:

--- routes/guestRoutes.js
+++ routes/guestRoutes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const cors = require('cors');
 const guestController = require('../controllers/guestController')
-const postController = require('../controllers/postController')
 router.use(cors())
 
 process.env.SECRET_KEY = 'secret'
@@ -23,10 +22,5 @@ router.route('/register')
 router.route('/:id')
   .get(guestController.getSingleMentor);
 
-  router.route('/posts')
-    .get(postController.getPosts)
-
-    router.route('/posts/:id')
-      .get(postController.getSinglePost)
     
 module.exports = router
--- routes/mentorRoutes.js
+++ routes/mentorRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const profileController = require('../controllers/profileController')
-const postController = require('../controllers/postController')
 const { isUserAuthenticated } = require('../middlewares/auth')
 
 //Edit Profile Route
@@ -11,8 +10,6 @@ router.route('/edit/:id', isUserAuthenticated)
   //Delete Profile Route
 router.route('/delete/:id', isUserAuthenticated)
   .delete(profileController.deleteProfile)
-  
-  router.route('/post/create', isUserAuthenticated)
-  .post(postController.createPost)
+
 
 module.exports = router
